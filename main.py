5d4055cc62fcc74165fd634abc04efab70bad730
mykola-ovchynnik <ovchynnik.mykola@gmail.com>
2025-02-09 17:46:47 +0100

    Binary search tree

---
+++ main.py
@@ -0,0 +1,97 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\t" * level + prefix + str(self.val) + "\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+
+def min_value_node(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current
+
+
+def max_value_node(node):
+    current = node
+    while current.right:
+        current = current.right
+    return current
+
+
+def delete(root, key):
+    if not root:
+        return root
+
+    if key < root.val:
+        root.left = delete(root.left, key)
+    elif key > root.val:
+        root.right = delete(root.right, key)
+    else:
+        if not root.left:
+            temp = root.right
+            root = None
+            return temp
+        elif not root.right:
+            temp = root.left
+            root = None
+            return temp
+        root.val = min_value_node(root.right).val
+        root.right = delete(root.right, root.val)
+    return root
+
+
+def sum_values(node):
+    if node is None:
+        return 0
+    return node.val + sum_values(node.left) + sum_values(node.right)
+
+
+root = Node(5)
+root = insert(root, 3)
+root = insert(root, 2)
+root = insert(root, 4)
+root = insert(root, 7)
+root = insert(root, 6)
+root = insert(root, 8)
+
+print(root)
+
+# task 1
+max_node = max_value_node(root)
+print(f"Maximum value: {max_node.val}")
+
+# task 2
+min_node = min_value_node(root)
+print(f"Minimum value: {min_node.val}")
+
+# task 3
+total_sum = sum_values(root)
+print(f"Sum of all values: {total_sum}")
